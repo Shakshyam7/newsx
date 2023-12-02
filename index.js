591ca27a8e09ac38391fe312b30a5a8ec4c93855
Shakshyam7 <90713152+Shakshyam7@users.noreply.github.com>
2023-12-02 13:25:24 -0800

    Removes the cached .env file and node modules

--- index.js
+++ index.js
@@ -3,5 +3,15 @@ const app = express();
 const db = require('mysql2');
 const dotenv = require('dotenv');
 
+const pool = mysql.createPool({
+  host: 'mariadb.bkoehler.imgd.ca', // Hostname of the database server
+  user: 'user',
+  password: 'password',
+  database: 'user0050_db0001',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  ssl: true,
+});
 
 
