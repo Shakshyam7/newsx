48790b1ad38a4f508bd2b915b1372cb09226611d
Shakshyam7 <90713152+Shakshyam7@users.noreply.github.com>
2023-12-05 18:26:29 -0800

    Hashes the password before saving to the db

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,19 @@
 import db from 'connect.js';
+import bcrypt from 'bcrypt';
 
 // Sign up the user
 export const signUp = async (req, res) => {
   const q = `SELECT FROM users WHERE email = ?`;
   db.query(q, [req.body.email], (err, data) => {
     if (err) return res.status(500).json(err);
-   if(data.length) return res.status(409).json("Email already in use")
+    else if (data.length) return res.status(409).json('Email already in use');
+    else {
+      // hash the password before saving to the db
+      const salt = bcrypt.genSaltSync(10);
+      const hashPw = bcrypt.hashSync(req.body.password, salt);
+      const q = `INSERT INTO users ('name', 'phone', 'email', password) VALUE(?)`;
+      const values = [req.body.name, req.body.phone, req.body.phone, hashPw]
+    }
   });
 };
 
