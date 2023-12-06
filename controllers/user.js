3229225d99071a041387bf2a197fda50f53cf7c2
Shakshyam7 <90713152+Shakshyam7@users.noreply.github.com>
2023-12-05 18:10:07 -0800

    Queries the db with the email and returns the response for err

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,13 @@
 import db from 'connect.js';
 
 // Sign up the user
-export const signUp = async (req, res) => {};
+export const signUp = async (req, res) => {
+  const q = `SELECT FROM users WHERE email = ?`;
+  db.query(q, [req.body.email], (err, data) => {
+    if (err) return res.status(500).json(err);
+   if(data.length) return res.status(409).json("Email already in use")
+  });
+};
 
 // Login the user
-export const login = async (req, res) => {
-  const q = `SELECT `;
-};
+export const login = async (req, res) => {};
