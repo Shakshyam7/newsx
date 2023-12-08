f93087196a42ea292dbd733c7ad23bfd16dac160
Shakshyam7 <90713152+Shakshyam7@users.noreply.github.com>
2023-12-08 15:47:31 -0800

    Adds the user info by querying the db in the res locals object

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,5 +1,7 @@
 import jwt from 'jsonwebtoken';
+import db from '../connect.js';
 
+// Verifies token
 const verifyToken = (req, res, next) => {
   const token = req.cookies.jwt;
   if (token) {
@@ -14,6 +16,35 @@ const verifyToken = (req, res, next) => {
     });
   } else {
     res.redirect('/login');
+  }
+};
+
+// Check User
+const checkUser = async (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, process.env.SECRET_KEY, (err, decodedToken) => {
+      if (err) {
+        console.log(err);
+        res.local.user = null;
+        next();
+      } else {
+        const q = `SELECT * FROM users WHERE id = ?`;
+        db.query(q, [decodedToken.id], (err, result) => {
+          if (err) {
+            console.log(err);
+            res.locals.user = null;
+          } else {
+            const user = result[0];
+            res.locals.user = user;
+          }
+        });
+        next();
+      }
+    });
+  } else {
+    res.locals.user = null;
+    next();
   }
 };
 
