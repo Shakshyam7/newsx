05ca7120f6e3a84baf94393199fd9fffc242d3c7
Shakshyam7 <90713152+Shakshyam7@users.noreply.github.com>
2023-12-11 21:19:44 -0800

    Adds the port

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
-app.listen(8000, () => {
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
   console.log(`server listenning on port 8000`);
 });
 
